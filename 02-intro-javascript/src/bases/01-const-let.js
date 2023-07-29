32b2ed950848db4088ae4f2472f1ee7d65f0b004
hfog2066 <hfog2000@yahoo.com>
2023-07-29 09:09:05 -0500

    Intro Java Script

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+// Variables y constantes  
+
+const nombre = "Hector";
+const apellido = "Orozco";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var no se debe usar.
+
+if ( true ){
+    const nombre = "Peter";
+    console.log(nombre)
+}
+
+console.log( valorDado);
+
